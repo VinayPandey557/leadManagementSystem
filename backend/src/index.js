53bb4e0a050f18715669b1a905e83ca071a2e1af
Vinay Pandey <vinaypandey46158@gmail.com>
2025-08-25 18:12:07 +0530

    Update backend CORS to allow deployed frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-       origin:"http://localhost:5173",
+       origin:"lead-management-system-ecru.vercel.app",
        credentials: true
     }));
 
